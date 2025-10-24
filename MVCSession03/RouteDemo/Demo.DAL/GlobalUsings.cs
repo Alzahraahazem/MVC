41cd01519518d63a545d2f2e1ee181cf65644851
Alzahraahazem <hazemzahraa1@gmail.com>
2025-10-24 17:27:07 +0300

    Delete MVCSession03/RouteDemo directory

--- MVCSession03/RouteDemo/Demo.DAL/GlobalUsings.cs
+++
@@ -1,3 +0,0 @@
-﻿global using  Demo.DAL.Models;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Metadata.Builders;
--- MVCSession03/RouteDemo/Demo.Session04/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Demo.Session04.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
